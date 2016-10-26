1a975e632fc03b986ac81fd8c68da3a3ea29c026
Alessandro Badin <javendo@gmail.com>
2016-10-26 01:24:43 -0200

    adding call stuff

--- build.sbt
+++ build.sbt
@@ -11,17 +11,22 @@ connectInput in run := true
 javaOptions += "-Djava.net.preferIPv4Stack=true"
 
 resolvers ++= Seq(
-   "Avaya Nexus" at "http://nexus.forge.avaya.com/content/groups/public",
-   "Maven Repo" at "http://repo.maven.apache.org/maven2"
+  "Avaya Nexus" at "http://nexus.forge.avaya.com/content/groups/public",
+  "Maven Repo" at "http://repo.maven.apache.org/maven2"
 )
 
-libraryDependencies ++= Seq(
-   "com.avaya" % "cmapi" % "5.2.4.60" exclude ("castor", "castor"),
-   "castor" % "castor" % "1.0-xml",
-   "xerces" % "xercesImpl" % "2.11.0",
-   "commons-logging" % "commons-logging" % "1.2",
-   "com.avaya.ph" % "ecsjtapia" % "5.2.0.540",
-   "log4j" % "log4j" % "1.2.17",
-   "com.typesafe.akka" % "akka-actor_2.11" % "2.4.2"
-)
+libraryDependencies ++= {
+  val akkaVersion = "2.4.9"
+  Seq(
+    "com.avaya" % "cmapi" % "5.2.4.60" exclude ("castor", "castor"),
+    "castor" % "castor" % "1.0-xml",
+    "xerces" % "xercesImpl" % "2.11.0",
+    "commons-logging" % "commons-logging" % "1.2",
+    "com.avaya.ph" % "ecsjtapia" % "5.2.0.540",
+    "log4j" % "log4j" % "1.2.17",
+    "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+    "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test",
+    "org.scalatest" %% "scalatest" % "2.2.0" % "test"
+  )
+}
 
--- src/main/scala/com/avaya/remoteagent/StationManagement.scala
+++ src/main/scala/com/avaya/remoteagent/StationManagement.scala
@@ -1,24 +1,20 @@
 package com.avaya.remoteagent
 
 import java.util.Properties
-import ch.ecma.csta.binding._
-import ch.ecma.csta.errors._
-import ch.ecma.csta.monitor._
-import ch.ecma.csta.voiceunit._
-import com.avaya.api.media.audio._
-import com.avaya.cmapi._
-import com.avaya.csta.async._
-import com.avaya.csta.binding._
-import com.avaya.csta.binding.types._
-import com.avaya.csta.device._
-import com.avaya.csta.physical._
-import com.avaya.csta.registration._
-import com.avaya.csta.tonecollection._
-import com.avaya.mvcs.framework._
+
 import akka.actor.Actor
+import ch.ecma.csta.binding._
+import ch.ecma.csta.physical._
+import com.avaya.cmapi._
+import com.avaya.csta.binding._
+import com.avaya.csta.device._
+import com.avaya.csta.terminal._
+import com.avaya.csta.physical._;
+import com.avaya.csta.registration.RegistrationServices
 
 object StationManagement {
   case class Station(number: Int)
+  case class MakeCall(deviceId: DeviceID)
   case class Register(station: Station)
   case class Unregister(station: Station)
 }
@@ -26,35 +22,80 @@ object StationManagement {
 class StationManagement extends Actor {
   import StationManagement._
 
+  val p = new Properties()
+  p.setProperty("cmapi.server_ip", "10.135.32.201")
+  p.setProperty("cmapi.username", "avaya")
+  p.setProperty("cmapi.password", "avayapassword")
+  p.setProperty("cmapi.server_port", "4721")
+  p.setProperty("cmapi.secure", "false")
+
   def receive = {
-    case Register(station) =>
-      val p = new Properties()
-      p.setProperty("cmapi.server_ip", "135.122.6.116")
-      p.setProperty("cmapi.username", "ctiuser")
-      p.setProperty("cmapi.password", "Ctiuser1!")
-      p.setProperty("cmapi.server_port", "4721")
-      p.setProperty("cmapi.secure", "false")
-      val serviceProvider = ServiceProvider.getServiceProvider(p)
+    case Register(station) => {
       val devRequest = new GetDeviceId()
-      devRequest.setSwitchIPInterface("135.122.41.48")
-      devRequest.setExtension("40004")
+      devRequest.setSwitchIPInterface("10.135.34.4")
+      devRequest.setExtension(station.toString)
       val deviceServices = serviceProvider.getService(classOf[DeviceServices].getName).asInstanceOf[DeviceServices]
       val devResponse = deviceServices.getDeviceID(devRequest)
       val deviceId = devResponse.getDevice()
-      val regRequest = new RegisterTerminalRequest();
-      regRequest.setDevice(deviceId);
-      val login = new LoginInfo();
-      login.setPassword("1234");
-      login.setSharedControl(false);
-      login.setTelecommuterExtension("40018");
-      regRequest.setLoginInfo(login);
-      regRequest.setLocalMediaInfo(null);
+      val regRequest = new RegisterTerminalRequest()
+      regRequest.setDevice(deviceId)
+      val login = new LoginInfo()
+      login.setPassword("123456")
+      login.setSharedControl(false)
+      regRequest.setLoginInfo(login)
+      regRequest.setLocalMediaInfo(null)
       val registrationServices = serviceProvider.getService(classOf[RegistrationServices].getName).asInstanceOf[RegistrationServices]
       val registerTerminal = registrationServices.registerTerminal(regRequest);
-      if (RegistrationConstants.NORMAL_REGISTER.equals(registerTerminal.getCode())) {
-	println("registered")
+      if (RegistrationConstants.NORMAL_REGISTER == registerTerminal.getCode()) {
+	sender ! deviceId
       }
-    case Unregister =>
+    }
+    case MakeCall(deviceId) => {
+      val physSvcs = serviceProvider.getService(classOf[PhysicalDeviceServices].getName).asInstanceOf[PhysicalDeviceServices]
+      pressButton(ButtonIDConstants.HOLD, deviceId, physSvcs)
+      pressButton("1"/*LAMP*/, deviceId, physSvcs)
+      pressButton("1", deviceId, physSvcs)
+    }
+    case Unregister => {
+      val termSvcs = serviceProvider.getService(classOf[TerminalServices].getName).asInstanceOf[TerminalServices]
+      val unregisterRequest = new UnregisterDevice();
+      //unregisterRequest.setDevice(id);
+      termSvcs.unregisterDevice(unregisterRequest)
       println("unregistered")
+    }
   }
+
+  val serviceProvider = ServiceProvider.getServiceProvider(p)
+
+  def pressButton(buttonCode: String, id: DeviceID, physSvcs: PhysicalDeviceServices) = {
+    val request = new ButtonPress()
+    var translatedButton: String = ""
+    if ("*".equals(buttonCode)) {
+      translatedButton = ButtonIDConstants.DIAL_PAD_STAR;
+    }
+    else if ("#".equals(buttonCode)) {
+      translatedButton = ButtonIDConstants.DIAL_PAD_POUND;
+    }
+    else {
+      translatedButton = buttonCode;
+    }
+    request.setDevice(id);
+    request.setButton(translatedButton);
+    // Send the request and catch any resulting CstaExceptions.
+    physSvcs.pressButton(request)
+  }
+
+  def getButtonInformation(id: DeviceID, physSvcs: PhysicalDeviceServices) = {
+    val buttonRequest = new GetButtonInformation()
+    buttonRequest.setDevice(id)
+    val buttonResponse = physSvcs.getButtonInformation(buttonRequest)
+    val list = buttonResponse.getButtonList()
+    val buttons = list.getButtonItem()
+    for (i <- 0 to buttons.length - 1) {
+      if (ButtonFunctionConstants.CALL_APPR == buttons(i).getButtonFunction()) {
+        //callAppearanceLamps.put(buttons[i].getButton(), LampModeConstants.OFF);
+      }
+    }
+  }
+
 }
